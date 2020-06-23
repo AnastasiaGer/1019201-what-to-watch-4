2dd696e7d0c760ddee75f201260e16369320cc19
Anisa <aagermanova@gmail.com>
2020-06-22 21:20:16 -0700

    Настраивает роутинг в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
+import MoviePage from "../movie-page/movie-page.jsx";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
-const App = (props) => {
-  const {movieCard, movies} = props;
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  const handleTitleClick = (evt) => {
-    evt.preventDefault();
-  };
+    this.state =
+      {
+        activeMovieCard: false
+      };
 
-  return (
-    <Main
-      movieCard={movieCard}
-      movies={movies}
-      onTitleClick={handleTitleClick} />
-  );
-};
+    this._handleTitleClick = this._handleTitleClick.bind(this);
+
+  }
+
+  _renderApp() {
+    const {movies, movieCard} = this.props;
+    if (this.state.activeMovieCard) {
+      return (
+        <MoviePage
+          movie={this.state.activeMovieCard}
+          movies={movies}
+          onTitleClick={this._handleTitleClick}
+        />
+      );
+    } else {
+      return (
+        <Main
+          movieCard={movieCard}
+          movies={movies}
+          onTitleClick={this._handleTitleClick}
+        />
+      );
+    }
+  }
+  render() {
+    const {movies} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-component">
+            <MoviePage
+              movie = {movies[0]}
+              movies = {movies}
+              onTitleClick={this._handleTitleClick}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _handleTitleClick(movie) {
+    this.setState({activeMovieCard: movie});
+  }
+}
 
 App.propTypes = {
   movieCard: PropTypes.shape({
